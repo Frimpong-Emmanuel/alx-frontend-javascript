edb9b9bf9cc2a7dbc3ef909597eb914ff9445f3a
Frimpong-Emmanuel <emmanuelfrimpong426@gmail.com>
2024-03-31 23:52:32 +0000

    corrections

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -8,8 +8,8 @@ export function getLast() {
 }
 
 export function taskNext() {
- let combination = 'But sometimes let';
+  let combination = 'But sometimes let';
   combination += getLast();
 
   return combination;
-)
+}
